8f89d9c3ed7de772cac71130292cd2b0b0a93aa8
sanat-buyya <sanatbuyya@gmail.com>
2025-04-22 12:56:01 +0530

    3. Registition Completed

--- src/main/java/com/jsp/ecommerce/helper/EmailSender.java
+++ src/main/java/com/jsp/ecommerce/helper/EmailSender.java
@@ -1,12 +1,16 @@
 package com.jsp.ecommerce.helper;
 
-import java.util.Random;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.jsp.ecommerce.dto.UserDto;
+
+import jakarta.mail.internet.MimeMessage;
 
 @Component
 public class EmailSender {
@@ -14,14 +18,30 @@ public class EmailSender {
 	@Autowired
 	JavaMailSender mailSender;
 
-	private int generateOTP() {
-		return new Random().nextInt(100000, 1000000);
-	}
+	@Autowired
+	TemplateEngine engine;
 
-	public void sendEmail(UserDto userDto) {
-		int otp = generateOTP();
+	@Value("${spring.mail.username}")
+	String from;
+
+	public void sendEmail(UserDto userDto, int otp) {
+
+		Context context = new Context();
+		context.setVariable("otp", otp);
+		context.setVariable("name", userDto.getName());
+
+		String text = engine.process("email-template.html", context);
+
 		try {
-			
+			MimeMessage message = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message);
+
+			helper.setFrom(from, "Ecommerce Application");
+			helper.setTo(userDto.getEmail());
+			helper.setSubject("OTP verification");
+			helper.setText(text, true);
+
+			mailSender.send(message);
 		} catch (Exception e) {
 			System.err.println("OTP Sending to Email Failed but the otp is : " + otp);
 		}
